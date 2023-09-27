f701c77c51d4fdc6fe44ed4fd40aa3b86aadc604
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-09-27 00:02:09 -0300

    Add files via upload

---
+++ 4 - Introducción a PHP/ej5/visitas.php
@@ -0,0 +1,12 @@
+<!-- Página que va a contener al contador de visitas -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contador de visitas</title>
+</head>
+<body>
+<?php include("contador.php");?>
+</body>
+</html>
